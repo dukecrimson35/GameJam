5cb108cb7f2c4b2d5f39705502a6d97f444abc12
MYCOMPUTER\K018A1452 <daikiiijima726@gmail.com>
2021-09-23 14:38:26 +0900

    ゲームシーンにキャラ配置

---
+++ gamejam/Assets/Wall.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Fish")
+        {
+            other.gameObject.transform.position = new Vector3(300,160,0);
+        }
+    }
+}
